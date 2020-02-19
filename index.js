c553164c853582c4b412f14463c5178562fb5bcd
Jherich Silas <jherichsilas@Jherichs-MacBook-Pro-7.local>
2020-02-19 04:23:46 -0600

    index.js didnt update, trying again.

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
+    
 'use strict';
 
 // put your own value below!
-const apiKey = 'AIzaSyD3eThebYAUpvw-2e_txqWGK1NYsMwF2JI'; 
-const searchURL = 'https://www.googleapis.com/youtube/v3/search';
+const apiKey = 'vLXnnFm5eegsmvjJpVaTsEpnPV5YQE4cdZeCBv7N'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
   return queryItems.join('&');
 }
 
 function displayResults(responseJson) {
   // if there are previous results, remove them
   console.log(responseJson);
-  $('#results-list').empty();
+  $('#results').empty();
   // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
+  for (let i = 0; i < responseJson.data.length; i++){
     // for each video object in the items 
     //array, add a list item to the results 
     //list with the video title, description,
     //and thumbnail
-    $('#results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+    $('#results').append(
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <span>
+      <a href='${responseJson.data[i].url}'>
+      ${responseJson.data[i].url}
+      </span>
       </li>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
-};
+    );
+  }
+ 
+}
 
-function getYouTubeVideos(query, maxResults=10) {
+function getListOfNps(parkCode, maxResults=10) {
   const params = {
-    key: apiKey,
-    q: query,
-    part: 'snippet',
-    maxResults,
-    type: 'video'
+    parkCode: parkCode,
+    api_key: apiKey,
+    //stateCode: stateCode,
+    //limit:maxResults   
   };
-  const queryString = formatQueryParams(params)
+  const queryString = formatQueryParams(params);
   const url = searchURL + '?' + queryString;
 
   console.log(url);
@@ -62,8 +65,9 @@ function watchForm() {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
-    getYouTubeVideos(searchTerm, maxResults);
+    getListOfNps(searchTerm, maxResults);
   });
 }
 
 $(watchForm);
+
